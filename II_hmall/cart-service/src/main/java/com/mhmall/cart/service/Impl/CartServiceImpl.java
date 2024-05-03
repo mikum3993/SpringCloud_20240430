29eaecf0683aedfa013b490b0010d81162da53d6
itcast <hello@itcast.cn>
2024-05-03 20:07:16 +0900

    commit;

--- II_hmall/cart-service/src/main/java/com/mhmall/cart/service/Impl/CartServiceImpl.java
+++ II_hmall/cart-service/src/main/java/com/mhmall/cart/service/Impl/CartServiceImpl.java
@@ -1,27 +1,32 @@
 package com.mhmall.cart.service.Impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.dto.ItemDTO;
 import com.hmall.common.exception.BizIllegalException;
 import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import com.mhmall.cart.doamin.dto.CartFormDTO;
-import com.mhmall.cart.doamin.dto.ItemDTO;
 import com.mhmall.cart.doamin.po.Cart;
 import com.mhmall.cart.doamin.vo.CartVO;
 import com.mhmall.cart.mapper.CartMapper;
 import com.mhmall.cart.service.ICartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -42,11 +47,15 @@ import java.util.stream.Collectors;
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
     //@Autowired    不推荐
-    private final RestTemplate restTemplate;
+   /*  private final RestTemplate restTemplate;
 
     // public CartServiceImpl(RestTemplate restTemplate) {
     //     this.restTemplate = restTemplate;
     // }
+
+    private final DiscoveryClient discoveryClient; */
+
+    private final ItemClient itemClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -95,10 +104,17 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 2.查询商品
 
         // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
-
+        // 2.1 .根据服务名称获取服务的实例列表
+       /*  List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        if (CollUtil.isEmpty(instances)) {
+            return;
+        }
+        // 2.2.手写负载均衡，从实例列表中挑选一个实例
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+        // URI uri = instance.getUri();
         // 2.1 利用RestTemplate 发起http请求，得到http 的响应
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
+                instance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {
@@ -111,6 +127,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         }
         // 2.2解析响应
         List<ItemDTO> items = response.getBody();
+        */
+
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
 
         if (CollUtils.isEmpty(items)) {
             return;
